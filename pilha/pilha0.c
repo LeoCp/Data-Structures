d2c23d94c84b41f1bee7b4ee42c4e07fb1ade6f9
Leonardo <leonardocolodette0@gmail.com>
2015-03-04 16:30:07 -0300

    6commit

---
+++ pilha/pilha0.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "pilha1.c"
+
+
+
+//Funções//////////
+void show(void);
+///////////////////
+
+main(){
+
+int q = 9, op,z,w;
+PILHA *p;
+
+do{
+	
+	system("clear");
+	show();
+	printf("Digite a opcao:");
+	scanf("%d",&op);
+	
+	switch(op){
+		
+		case 1:
+			printf("Digite o tamanho da pilha:\n");scanf("%d",&z);
+		 	p = CriarPilha(z);
+		break;
+
+		case 2:
+			printf("Inserindo..\n");
+			scanf("%d",&w);
+			InserirPilha(p,w);
+		break;
+
+		case 3:
+		break;
+
+		case 4:
+			MostrarPilha(p);
+			sleep(5);
+		break;
+
+		case 5:
+			printf("Saindo\n");
+		break;
+
+	}
+
+}while(op != 5);
+
+
+
+}
+
+void
+show(void){
+	printf("\n++++++++++++++++++++++++++++++++++++++++++\n");
+	printf("\t         PILHA :]          \n");
+	printf("------------------------------------------\n");
+	printf(" 1 - Criar uma pilha\n 2 - Inserir  \n 3 - Remover\n 4 - Mostra \n 5 - SAIR\n");
+	printf("++++++++++++++++++++++++++++++++++++++++++\n");
+
+}
---
+++ pilha/pilha1.c
@@ -0,0 +1,48 @@
+struct pilha{
+	int *v;
+	int topo;
+	int tamanho;
+};
+
+typedef struct pilha PILHA;
+
+PILHA *CriarPilha(int);
+void InserirPilha(PILHA*,int);
+void MostrarPilha(PILHA*);
+int RetirarPilha(PILHA*);
+
+PILHA *CriarPilha(int x){
+	
+	PILHA *y;
+	y =  (PILHA*) malloc( sizeof(PILHA));
+	y->v = (int*) malloc(x * sizeof(int));
+	y->topo = 0;
+	y->tamanho = x;
+}	
+
+void InserirPilha(PILHA *x, int y){
+
+    if(x->topo < x->tamanho){
+	
+		x->v[x->topo] = y;
+		x->topo++;
+	
+	}
+
+}
+
+void MostrarPilha(PILHA *w){
+	int i;
+	for (i = 0; i < w->tamanho; ++i){
+		printf("%d\n",w->v[i]);
+	}
+}
+
+
+int RetirarPilha(PILHA *d){
+
+	/*code*/
+
+}
+
+
