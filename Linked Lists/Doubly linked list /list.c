8f9c6127b2e78ae69a315be49e6b103e714e4467
Leonardo <leonardocolodette0@gmail.com>
2015-11-19 23:55:54 -0200

    28comm

--- Linked Lists/Doubly linked list /list.c
+++ Linked Lists/Doubly linked list /list.c
@@ -1,8 +1,7 @@
 /*****************************
 * Leonardo Colodette
-* Doubly Linked Lists
+* Linked Lists
 ****************************/
-
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -19,22 +18,52 @@ Node *newNode(int);
 Node *addNode(Node*,Node*);
 void showNode(Node*);
 void showList(Node*);
-void removeNode(Node*,int);
+void removeNode(Node*,int,int);
 void alterNode (Node*,int,int);
-
+void design (int);
 
 main(){
 
-  Node *x = NULL;
-  int y;
+  int m,v,s,r;
+  Node *n = NULL;
 
-  scanf("%d",&y);
-  while(y != 0){
-    x = addNode(x,newNode(y));
-    scanf("%d",&y);
+  while(m != 5){
+    system("clear");design(1);
+    printf("Opção: ");scanf("%d",&m);
+
+    switch (m) {
+
+      case 1:
+        system("clear");design(2);
+        printf("Valor: ");scanf("%d",&v);
+        n = addNode(n,newNode(v));
+      break;
+
+      case 2:
+        system("clear");design(3);
+        showList(n);design(4);
+        printf("Sair[0/1]: ");scanf("%d",&s);
+      break;
+
+      case 3:
+
+        system("clear");design(5);
+        showList(n);design(4);
+        printf("Qual valor? ");scanf("%d",&s);
+        printf("Qual indice? ");scanf("%d",&r);
+
+      break;
+
+      case 4:
+        printf("op 4\n");
+      break;
+      case 5:
+        printf("Bey\n");
+      break;
+    }
   }
 
-showList(x);
+
 }
 
 Node *newNode(int number){
@@ -59,7 +88,7 @@ Node *addNode(Node *root, Node *next){
 }
 
 void showNode(Node *node){
-  printf("%d\n",node->value);
+  printf("| %d             |\n",node->value);
 }
 
 void showList(Node *z){
@@ -68,5 +97,45 @@ void showList(Node *z){
     showList(z->next);
   }
 }
-void removeNode(Node *f,int number){}
+void removeNode(Node *f,int number, int inde){
+}
 void alterNode (Node *h ,int ant,int nov ){}
+
+void design (int x){
+  switch (x) {
+    case 1:
+      printf("-----------------\n");
+      printf("|      Menu     |\n");
+      printf("|---------------|\n");
+      printf("| 1 - Adicionar |\n");
+      printf("| 2 - Mostrar   |\n");
+      printf("| 3 - Remover   |\n");
+      printf("| 4 - Alterar   |\n");
+      printf("| 5 - Sair      |\n");
+      printf("-----------------\n");
+    break;
+
+    case 2:
+      printf("-----------------\n");
+      printf("|    Cadastar   |\n");
+      printf("-----------------\n");
+    break;
+
+    case 3:
+    printf("-----------------\n");
+    printf("|     Lista     |\n");
+    printf("-----------------\n");
+
+
+    break;
+    case 4:
+    printf("-----------------\n");
+    break;
+
+    case 5:
+    printf("-----------------\n");
+    printf("|     Remover   |\n");
+    printf("-----------------\n");
+    break;
+  }
+}
